4f82b757e7a38104f17ee63fd4d3de7aba66516f
alexnm <alex.n.moldovan@gmail.com>
2017-11-06 21:27:57 +0200

    removed old line

--- app/components/updates/index.js
+++ app/components/updates/index.js
@@ -11,7 +11,6 @@ const Updates = () => (
         <li><b>Speakers:</b> Call for Speakers for 2018 is now open! <b><Link href="https://docs.google.com/forms/d/e/1FAIpQLSdPSo4Zy_M78PHRVFz90v1_SV3IqRcyhvgK2oCDY6ju6NDWeA/viewform?usp=sf_link"><a target="_blank">Register your Talk ideas</a></Link></b> and we’ll get back to you soon!</li>
         <li><b>Sponsors:</b> Our Call for Sponsors is always open. Send us an email if you’re interested to join our mission in 2018 <b><Link href="maito:welcome@jsheroes.io"><a>welcome@jsheroes.io</a></Link></b>, and we’ll take it from there.</li>
         <li><b>Wanted:</b> Passionate community members: Join the JSHeroes organizing crew! Get in touch and let us know what you’d like to contribute with.</li>
-        <li><b>Register your interest</b> for JSHeroes 2018 and you’ll be the first to know when we’ll launch the Early Bird Tickets. <b><Link href="https://ti.to/cluj-javascripters/jsheroes2018"><a target="_blank">Click here.</a></Link></b></li>
       </ul>
     </div>
 
