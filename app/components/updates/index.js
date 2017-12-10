b6820c788dce3c370a224300f937e9a64a804c92
alexnm <alex.n.moldovan@gmail.com>
2017-12-10 17:01:04 +0200

    updates to the updates section refs #20

--- app/components/updates/index.js
+++ app/components/updates/index.js
@@ -5,14 +5,17 @@ import Section from '../../components/common/section';
 const Updates = () => (
   <Section>
     <div className="wrapper">
-      <h1>Updates</h1>
-      <ul>
-        <li><b>Date and venue:</b> Join us on 18-20 April 2018 in Cluj Napoca, Romania at Grand Hotel Italia for 1 day of workshops and 2 days of conference. Feel free to send us your suggestions over Twitter, we’d be happy to hear from you.</li>
-        <li><b>Speakers:</b> Call for Speakers for 2018 is now open! <b><Link href="https://docs.google.com/forms/d/e/1FAIpQLSdPSo4Zy_M78PHRVFz90v1_SV3IqRcyhvgK2oCDY6ju6NDWeA/viewform?usp=sf_link"><a target="_blank">Register your Talk ideas</a></Link></b> and we’ll get back to you soon!</li>
-        <li><b>Sponsors:</b> Our Call for Sponsors is always open. Send us an email if you’re interested to join our mission in 2018 <b><Link href="maito:welcome@jsheroes.io"><a>welcome@jsheroes.io</a></Link></b>, and we’ll take it from there.</li>
-        <li><b>Wanted:</b> Passionate community members: Join the JSHeroes organizing crew! Get in touch and let us know what you’d like to contribute with.</li>
-        <li><b>Tickets:</b> If PayPal is not your preferred payment method, you can also pay by invoice. Drop us a line at <b><Link href="maito:contact@jsheroes.io"><a>contact@jsheroes.io</a></Link></b> with your request, we’d be happy to help.</li>
-      </ul>
+      <h1>News</h1>
+      <strong>Date and Venue</strong>
+      <p>Join us on 18-20 April 2018 in Cluj Napoca, Romania at Grand Hotel Italia for 1 day of workshops and 2 days of conference. Feel free to send us your suggestions over <a href="https://twitter.com/jsheroes" target="_blank" rel="noreferrer noopener">Twitter</a>, we’d be happy to hear from you.</p>
+      <strong>Speakers</strong>
+      <p>Call for Papers is still open! <a href="https://docs.google.com/forms/d/e/1FAIpQLSdPSo4Zy_M78PHRVFz90v1_SV3IqRcyhvgK2oCDY6ju6NDWeA/viewform?usp=sf_link" target="_blank" rel="noreferrer noopener">Submit your proposals</a> until the 30th of December!</p>
+      <strong>Sponsors</strong>
+      <p>Our Call for Sponsors is always open. Send us an email if you’re interested to join our mission in 2018 at <a href="maito:welcome@jsheroes.io">welcome@jsheroes.io</a> and we’ll take it from there.</p>
+      <strong>Wanted</strong>
+      <p>Passionate community members: Join the JSHeroes organizing crew! Get in touch and let us know what you’d like to contribute with.</p>
+      <strong>Tickets</strong>
+      <p>If PayPal is not your preferred payment method, you can also pay by invoice. Drop us a line at <a href="maito:contact@jsheroes.io">contact@jsheroes.io</a> with your request, we’d be happy to help.</p>
     </div>
 
     <style jsx>{`
@@ -21,7 +24,15 @@ const Updates = () => (
           margin: 15px 0;
         }
         .wrapper {
-          margin: 100px 0;
+          margin: 100px auto;
+          text-align: center;
+          max-width: 900px;
+        }
+        .wrapper p {
+          margin-bottom: 20px;
+        }
+        .wrapper a {
+          font-weight: bold;
         }
         h1 {
           margin: 20px 0;
