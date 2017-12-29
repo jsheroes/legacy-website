0ba7b1c725f5212ac2038a3cd583b5c295c0a443
alexnm <alex.n.moldovan@gmail.com>
2017-12-29 18:05:41 +0200

    fix build

--- app/components/venue/google.map.js
+++ app/components/venue/google.map.js
@@ -1,63 +1,60 @@
-import { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 import MapPin from './google.map.pin';
 import { ConferenceMapApiKey, mediaQueries } from '../../constants';
 
-export default class GoogleMap extends Component {
-  static defaultProps = {
-    center: { lat: 46.752870, lng: 23.605845 },
-    zoom: 16,
-    zoomControl: true,
-    panControl: false,
-    streetViewControl: false,
-    mapTypeControl: true,
-    overviewMapControl: false,
-    clickable: true,
-  }
+const GoogleMap = () => (
+  <div>
+    <GoogleMapReact
+      defaultCenter={this.props.center}
+      defaultZoom={this.props.zoom}
+      bootstrapURLKeys={{ key: ConferenceMapApiKey }}
+      options={createMapOptions}
+    >
+      <MapPin lat={46.752870} lng={23.605845} />
+    </GoogleMapReact>
 
-  render() {
-    return (
-      <div>
-        <GoogleMapReact
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-          bootstrapURLKeys={{ key: ConferenceMapApiKey }}
-          options={createMapOptions}
-        >
-          <MapPin lat={46.752870} lng={23.605845} />
-        </GoogleMapReact>
+    <style jsx>{`
+      div {
+        position: relative;
+        width: 100%;
+        height: 666px;
+        overflow: hidden;
+      }
+      
+      @media (max-width: ${mediaQueries.XL}) and (min-width: ${mediaQueries.L}) {
+        div {
+          height: 596px;
+        }
+      }
 
-        <style jsx>{`
-          div {
-            position: relative;
-            width: 100%;
-            height: 666px;
-            overflow: hidden;
-          }
-          
-          @media (max-width: ${mediaQueries.XL}) and (min-width: ${mediaQueries.L}) {
-            div {
-              height: 596px;
-            }
-          }
-    
-          @media (max-width: ${mediaQueries.L}) and (min-width: ${mediaQueries.S}) {
-            div {
-              height: 494px;
-            }
-          }
-    
-          @media only screen and (max-width: ${mediaQueries.S}) {
-            div {
-              height: 310px;
-            }
-          }
-        `}</style>
-      </div>
-    );
-  }
-}
+      @media (max-width: ${mediaQueries.L}) and (min-width: ${mediaQueries.S}) {
+        div {
+          height: 494px;
+        }
+      }
+
+      @media only screen and (max-width: ${mediaQueries.S}) {
+        div {
+          height: 310px;
+        }
+      }
+    `}</style>
+  </div>
+);
+
+GoogleMap.defaultProps = {
+  center: { lat: 46.752870, lng: 23.605845 },
+  zoom: 16,
+  zoomControl: true,
+  panControl: false,
+  streetViewControl: false,
+  mapTypeControl: true,
+  overviewMapControl: false,
+  clickable: true,
+};
+
+export default GoogleMap;
 
 function createMapOptions(maps) {
   return {
