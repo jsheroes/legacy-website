391e371890600c64322f5c87d29a6b3cf3b0d360
alexnm <alex.n.moldovan@gmail.com>
2018-02-02 18:02:52 +0200

    data update

--- app/data/sponsors.js
+++ app/data/sponsors.js
@@ -24,49 +24,19 @@ module.exports = [
     img: 'logo_yonder.png',
     href: 'https://tss-yonder.com/',
   },
-  // {
-  //   title: 'Informal School of IT',
-  //   img: 'logo_scoala_informala.jpg',
-  //   href: 'http://scoalainformala.ro/ ',
-  // },
+  {
+    title: 'Moqups',
+    img: 'logo_moqups.jpg',
+    href: 'https://moqups.com/',
+  },
+  {
+    title: 'Imprezzio',
+    img: 'logo_imprezzio1.png',
+    href: 'http://imprezzioglobal.com/',
+  },
   // {
   //   title: 'Nielsen Marketing Cloud',
   //   img: 'logo_nielsen_mc.png',
   //   href: 'http://www.nielsen.com/eu/en/solutions/capabilities/nielsen-marketing-cloud.html',
-  // },
-  // {
-  //   title: 'Google Developers',
-  //   img: 'logo_google_dev_exp.png',
-  //   href: 'https://developers.google.com/',
-  // },
-  // {
-  //   title: 'Moqups',
-  //   img: 'logo_moqups.jpg',
-  //   href: 'https://moqups.com/',
-  // },
-  // {
-  //   title: 'LogMeIn',
-  //   img: 'logo_log_me_in.png',
-  //   href: 'https://www.logmeininc.com/',
-  // },
-  // {
-  //   title: 'Connatix',
-  //   img: 'logo_connatix.png',
-  //   href: 'https://connatix.com/',
-  // },
-  // {
-  //   title: 'Accenture',
-  //   img: 'logo_accenture.png',
-  //   href: 'https://www.accenture.com/',
-  // },
-  // {
-  //   title: 'Spyhce',
-  //   img: 'logo_spyhce.png',
-  //   href: 'http://www.spyhce.com/',
-  // },
-  // {
-  //   title: 'Toptal',
-  //   img: 'logo_toptal.png',
-  //   href: 'https://www.toptal.com/',
   // },
 ];
