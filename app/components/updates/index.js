87d35768b61629564b2d805ee3c3605e0ddf751d
Daniel Mocan <contact_daniel@yahoo.com>
2018-03-06 13:25:33 +0200

    Moved news information to data folder

--- app/components/updates/index.js
+++ app/components/updates/index.js
@@ -1,33 +1,6 @@
 import { Component } from 'react';
 import Section from '../../components/common/section';
-
-
-const newsData = [
-  {
-    title: 'Date and Venue ',
-    content: 'Join us on 18-20 April 2018 in Cluj Napoca, Romania at Grand Hotel Italia for 1 day of workshops and 2 days of conference. Feel free to send us your suggestions over <a href="https://twitter.com/jsheroes" target="_blank" rel="noreferrer noopener">Twitter</a>, we’d be happy to hear from you.',
-  },
-  {
-    title: 'Sponsors',
-    content: 'Our Call for Sponsors is always open. Send us an email if you’re interested to join our mission in 2018 at <a href="mailto:welcome@jsheroes.io">welcome@jsheroes.io</a> and we’ll take it from there.',
-  },
-  {
-    title: 'Volunteers',
-    content: 'Passionate community members: Join the JSHeroes organizing crew! Get in touch and let us know what you’d like to contribute with.',
-  },
-  {
-    title: 'Tickets',
-    content: 'If PayPal is not your preferred payment method, you can also pay by invoice. Drop us a line at <a href="mailto:contact@jsheroes.io">contact@jsheroes.io</a> with your request, we’d be happy to help.',
-  },
-  {
-    title: 'Adopt a Javascripter',
-    content: `If you want to be &quot;adopted&quot; by one of our <span className="sticky-words">Cluj Javascripters</span> during the <span className="sticky-words">JSHeroes conference</span>, you can use our&nbsp;
-      <a title="Adopt a Javascripter couch-surfing program" href="/adopt-a-jser">
-        <span className="sticky-words">Adopt a Javascripter</span> <span className="sticky-words">couch-surfing program</span>
-      </a>`,
-  },
-];
-
+import news from '../../data/news';
 
 class Updates extends Component {
   constructor() {
@@ -38,7 +11,7 @@ class Updates extends Component {
   }
 
   render() {
-    const news = newsData.map((item, index) => {
+    const newsData = news.map((item, index) => {
       const activeCss = index === this.state.activeNews ? 'active-news' : 'news-item';
       return (
         <li className={activeCss} onClick={() => this.setState({ activeNews: index })} key={item.title}>
@@ -46,13 +19,13 @@ class Updates extends Component {
         </li>
       );
     });
-    const content = newsData[this.state.activeNews].content;
+    const content = news[this.state.activeNews].content;
     return (
       <Section>
         <div className="wrapper">
           <h1>News</h1>
           <ul>
-            { news }
+            { newsData }
           </ul>
           <div className="content" dangerouslySetInnerHTML={{ __html: content }} />
         </div>
