aa2163552b542df1c3b57777d9bf09dc0350cd17
Alex Moldovan <alex.n.moldovan@gmail.com>
2019-05-21 09:02:56 +0300

    chore: update to now 2.0

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   webpack: null,
   poweredByHeader: false,
   assetPrefix: '',
+  target: 'serverless',
   serverRuntimeConfig: {},
   publicRuntimeConfig: {
     // Will be available on both server and client
