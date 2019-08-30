04e1c89eebbbf3c46e1067cbd2beaa8e223b9aa5
Alex Moldovan <alex.n.moldovan@gmail.com>
2019-08-30 09:26:14 +0300

    data: new stats

--- app/data/2020/speakers.js
+++ app/data/2020/speakers.js
@@ -1,1 +1,114 @@
-export default [];
+export default [
+  {
+    name: 'peggy',
+    fullName: 'Peggy Rayzis',
+    permalink: 'peggy-rayzis',
+    firstName: 'Peggy',
+    reference: 'her',
+    position: 'Developer Experience',
+    company: 'Apollo',
+    img: 'peggy-rayzis.jpg',
+    twitter: 'https://twitter.com/peggyrayzis',
+    description: '',
+    talk: {
+      title: 'TBA',
+      description: 'TBA',
+    },
+    workshop: null,
+    previousTalks: [],
+    otherTalks: [],
+  },
+  {
+    name: 'christian',
+    fullName: 'Christian Nwamba',
+    permalink: 'christian-nwamba',
+    firstName: 'Christian',
+    reference: 'him',
+    position: 'Senior Cloud Developer Advocate',
+    company: 'Microsoft',
+    img: 'christian-nwamba.jpg',
+    twitter: 'https://twitter.com/codebeast',
+    website: 'https://www.codebeast.dev/',
+    description: `My name is Christian Nwamba, also called Codebeast. I’m a Software Engineer and love making things that might possibly work using JavaScript. I currently work as a Senior Cloud Developer Advocate🥑 at Microsoft. I was also a Developer Advocate for 2 years at Cloudinary before joining Microsoft.
+
+    Before becoming an advocate, I loved working as a consultant. I consulted as a Software Developer, Developer Advocate, Technical Writer and Community Champion for startups including Scotch.io, Auth0, Pusher, Cloudinary, Progress, deepstreamHub, Logrocket, Dialog Engineers, Manifold, etc.
+    
+    I love working with the developer communities to build an ecosystem. With forces from colleagues and friends we built communities like forLoop Africa, Angular Nigeria, Concatenate Conference, Me.io, Frontstack.io and a lot more. I am responsible for expanding the forLoop Africa communities across new African countries and ecosystems while trying my best to sustain the existing ones.`,
+    talk: {
+      title: 'TBA',
+      description: 'TBA',
+    },
+    workshop: null,
+    previousTalks: [],
+    otherTalks: [],
+  },
+  {
+    name: 'tatiana',
+    fullName: 'Tatiana Mac',
+    permalink: 'tatiana-mac',
+    firstName: 'Tatiana',
+    reference: 'her',
+    position: 'Independent Designer',
+    company: '',
+    img: 'tatiana-mac.jpg',
+    twitter: 'https://twitter.com/TatianaTMac',
+    website: 'https://tatianamac.com/',
+    description: `Tatiana Mac is an independent American designer who works directly with organisations to build clear and coherent products and design systems.
+
+    She believes the trifecta of accessibility, performance, and inclusion can work symbiotically to improve our social landscape digitally and physically. When ethically-minded, she thinks technologists can dismantle exclusionary systems in favour of community-focused, inclusive ones.
+    
+    Never totally pleased with design tools, she designs in browser to bring performant, semantic, and accessible visual narratives into the web. Her current obsessions are optimising variable fonts, converting raster images into to SVGs, and recreating modernist paintings in CSS grid. When she can successfully escape vim, she finds new countries to explore (33 and counting).`,
+    talk: {
+      title: 'TBA',
+      description: 'TBA',
+    },
+    workshop: null,
+    previousTalks: [],
+    otherTalks: [],
+  },
+  {
+    name: 'jeremy',
+    fullName: 'Jeremy Keith',
+    permalink: 'jeremy-keith',
+    firstName: 'Jeremy',
+    reference: 'him',
+    position: 'Founder',
+    company: 'Clearleft',
+    img: 'jeremy-keith.jpg',
+    twitter: 'https://twitter.com/adactio',
+    website: 'https://adactio.com/',
+    description: `<p>Jeremy Keith lives in Brighton, England where he makes websites with the splendid design agency <a href="https://clearleft.com/">Clearleft</a>. You may know him from such books as <a href="https://domscripting.com/">DOM Scripting</a>, <a href="https://bulletproofajax.com/">Bulletproof Ajax</a>, <a href="https://html5forwebdesigners.com/">HTML5 For Web Designers</a>, <a href="https://resilientwebdesign.com/">Resilient Web Design</a>, and, most recently, <a href="https://abookapart.com/products/going-offline">Going Offline</a>.</p>
+    <p>He curated the dConstruct conference for a number of years as well as <a href="http://brightonsf.adactio.com/">Brighton SF</a>, and he organised the world's first <a href="http://sciencehackday.org/">Science Hack Day</a>. He also made the website <a href="https://huffduffer.com/">Huffduffer</a> to allow people to make podcasts of found sounds—it's like Instapaper for audio files.</p>
+    <p>Hailing from Erin's green shores, Jeremy maintains his link to Irish traditional music running the community site <a href="https://thesession.org/">The Session</a>. He also indulges a darker side of his bouzouki-playing in the band <a href="https://saltercane.com/">Salter Cane</a>.</p>
+    <p>Jeremy spends most of his time goofing off on the internet, documenting his time-wasting on <a href="https://adactio.com/">adactio.com</a>, where he has been writing for over fifteen years.</p>
+    `,
+    talk: {
+      title: 'TBA',
+      description: 'TBA',
+    },
+    workshop: null,
+    previousTalks: [],
+    otherTalks: [],
+  },
+  {
+    name: 'monica',
+    fullName: 'Monica Lent',
+    permalink: 'monica-lent',
+    firstName: 'Monica',
+    reference: 'her',
+    position: 'Frontend Lead ',
+    company: 'SumUp Engineering',
+    img: 'monica-lent.jpg',
+    twitter: 'https://twitter.com/monicalent',
+    website: 'https://monicalent.com',
+    github: 'https://github.com/mlent',
+    description: `Monica is an American software engineer living and working in Berlin. She began coding at the age of ten, and during her professional technical career worked in domains such as cloud services, natural language processing, e-learning, and fintech. Her most recent role has been leading a distributed team of frontend engineers at SumUp, a German fintech company, in areas such as architecture and technical strategy. In her spare time, she's bootstrapping an analytics startup.`,
+    talk: {
+      title: 'TBA',
+      description: 'TBA',
+    },
+    workshop: null,
+    previousTalks: [],
+    otherTalks: [],
+  },
+];
