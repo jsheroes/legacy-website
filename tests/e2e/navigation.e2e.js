56872b950b3e16ee9eb4e7604cae81082874f631
utwo <mihai.legat@gmail.com>
2019-10-13 14:54:30 +0300

    Update and fix cypress

--- tests/e2e/navigation.e2e.js
+++ tests/e2e/navigation.e2e.js
@@ -12,17 +12,5 @@ describe('Website navigation', () => {
         .click();
       cy.get(selectors.speakersSection).should('contain', 'Our 2020 Heroes');
     });
-
-    it('past editions', () => {
-      cy.get(selectors.menuItems)
-        .contains('Past Editions')
-        .click();
-      cy.get(selectors.linkLabel)
-        .first()
-        .should('contain', '2019');
-      cy.get(selectors.linkLabel)
-        .last()
-        .should('contain', '2017');
-    });
   });
 });
