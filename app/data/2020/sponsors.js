374c62267bce124a5a0d038744e8f6bd07834751
Alex Moldovan <alex.n.moldovan@gmail.com>
2019-10-19 17:21:17 +0300

    data: sponsors

--- app/data/2020/sponsors.js
+++ app/data/2020/sponsors.js
@@ -4,4 +4,14 @@ export default [
     img: 'logo_teleporthq.png',
     href: 'https://teleporthq.io/',
   },
+  {
+    title: 'Fortech',
+    img: 'logo_fortech.png',
+    href: 'http://www.fortech.ro/',
+  },
+  {
+    title: 'Evozon',
+    img: 'logo_new_evozon.png',
+    href: 'https://www.evozon.com/',
+  },
 ];
